fdfcb1c0969d66819b66c3cfdc47954d0f172866
Mircea-George Cozarev <mircea2501@gmail.com>
2022-10-29 16:33:12 +0300

    Create menu

--- finite-state-machine2/src/Main.java
+++ finite-state-machine2/src/Main.java
@@ -1,25 +1,112 @@
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
+    private static boolean finished;
+    private static FiniteStateMachine finiteStateMachine;
     public static void main(String[] args) {
+        run();
+    }
+
+    private static void printMenu() {
+        String[] menu = {
+                "Menu:",
+                "1.States",
+                "2.Alphabet",
+                "3.Initial state",
+                "4.Final states",
+                "5.Transitions",
+                "6.Verify if a sequence is accepted",
+                "7.Longest accepted prefix",
+                "0.Exit"
+        };
+        for (String s : menu) {
+            System.out.println(s);
+        }
+    }
+
+    private static void run() {
         String fileName = "resources\\finite-state-machine-input\\input1.in";
         try {
-            FiniteStateMachine finiteStateMachine = FiniteStateMachine.readFromFile(fileName);
-            finiteStateMachine.getStates();
-
-            String goodSequence = "cd";
-            String badSequence = "ca";
-//            System.out.println(finiteStateMachine.acceptsSequence(goodSequence));
-//            System.out.println(finiteStateMachine.acceptsSequence(badSequence));
-//            System.out.println(finiteStateMachine.acceptsSequence("d"));
-//            System.out.println(finiteStateMachine.acceptsSequence("abcd"));
-//            System.out.println(finiteStateMachine.acceptsSequence("aaa"));
-//            System.out.println(finiteStateMachine.acceptsSequence("cc"));
-
-            System.out.println(finiteStateMachine.longestPrefixAccepted("da"));
-            System.out.println(finiteStateMachine.longestPrefixAccepted("dddddddda"));
+            finiteStateMachine = FiniteStateMachine.readFromFile(fileName);
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+        finished = false;
+        while (!finished) {
+            printMenu();
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
+            if (Objects.equals(cmd, "0")) {
+                finished = true;
+            } else if (Objects.equals(cmd, "1")) {
+                printStates();
+            } else if (Objects.equals(cmd, "2")) {
+                printAlphabet();
+            } else if (Objects.equals(cmd, "3")) {
+                printInitialState();
+            } else if (Objects.equals(cmd, "4")) {
+                printFinalStates();
+            } else if (Objects.equals(cmd, "5")) {
+                printTransitions();
+            } else if (Objects.equals(cmd, "6")) {
+                checkSequence();
+            } else if (Objects.equals(cmd, "7")) {
+                printLongestAcceptedPrefix();
+            } else {
+                printWrongCommand();
+            }
+        }
+    }
+
+    private static void printWrongCommand() {
+        System.out.println("Invalid command!");
+    }
+
+    private static void printLongestAcceptedPrefix() {
+        System.out.println("Enter a sequence to find its longest accepted prefix:");
+        Scanner scanner = new Scanner(System.in);
+        String sequence = scanner.nextLine().strip();
+        System.out.println(finiteStateMachine.longestPrefixAccepted(sequence));
+    }
+
+    private static void checkSequence() {
+        System.out.println("Enter a sequence to validate:");
+        Scanner scanner = new Scanner(System.in);
+        String sequence = scanner.nextLine().strip();
+        System.out.println(finiteStateMachine.acceptsSequence(sequence) ? "Valid" : "Invalid");
+    }
+
+    private static void printTransitions() {
+        System.out.println("Transitions:");
+        finiteStateMachine.getTransitions()
+                .forEach(tr -> System.out.println(tr[0] + "--" + tr[1] + "->" + tr[2]));
+    }
+
+    private static void printFinalStates() {
+        System.out.print("Final states: ");
+        Arrays.stream(finiteStateMachine.getFinalStates())
+                .forEach(x -> System.out.print(x + " "));
+        System.out.println();
+    }
+
+    private static void printInitialState() {
+        System.out.println("Initial state: " + finiteStateMachine.getInitialState());
+    }
+
+    private static void printAlphabet() {
+        System.out.print("Symbols alphabet: ");
+        Arrays.stream(finiteStateMachine.getAlphabet())
+                .forEach(x -> System.out.print(x + " "));
+        System.out.println();
+    }
+
+    private static void printStates() {
+        System.out.print("States alphabet: ");
+        Arrays.stream(finiteStateMachine.getStates())
+                .forEach(x -> System.out.print(x + " "));
+        System.out.println();
     }
 }
